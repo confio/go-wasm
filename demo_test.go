36c9437e389d1a550a666e56e1dfe30e29b2ffb1
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-15 12:37:52 +0200

    Run tests off examples folder, including with args

--- demo_test.go
+++ demo_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	input, err := ioutil.ReadFile("wasm/fib_recursive.wasm")
+	input, err := ioutil.ReadFile("examples/fib_recursive/build/fib_recursive.wasm")
 	if err != nil {
 		t.Fatalf("%+v", err)
 	}
@@ -16,6 +16,14 @@ func TestRun(t *testing.T) {
 		t.Fatalf("%+v", err)
 	}
 	if res != 9227465 { // fib(35)
+		t.Fatalf("Unexpected result: %d", res)
+	}
+
+	res, err = Run(input, "fib", []int64{8})
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	if res != 21 { // fib(8)
 		t.Fatalf("Unexpected result: %d", res)
 	}
 }
