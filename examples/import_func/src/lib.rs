4e4d2700d8923425cf836bfc1a64a1afc05cf45b
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-15 16:41:04 +0200

    Pass string into import func

--- examples/import_func/src/lib.rs
+++ examples/import_func/src/lib.rs
@@ -1,8 +1,15 @@
 extern "C" {
     fn sum(x: i32, y: i32) -> i32;
+    fn repeat(pointer: *const u8, length: u32, count: i32);
 }
 
 #[no_mangle]
 pub extern "C" fn add1(x: i32, y: i32) -> i32 {
-    unsafe { sum(x, y) + 1 }
+    let msg = "fool ";
+
+    unsafe { 
+        let cnt = sum(x, y) + 1;
+        repeat(msg.as_ptr(), msg.len() as u32, cnt);
+        return cnt;
+    }
 }
