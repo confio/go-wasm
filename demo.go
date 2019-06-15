d1f60d3ce17400d040f554e16df37b517e9ecf05
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-15 15:18:50 +0200

    Prepare string input to greet

--- demo.go
+++ demo.go
@@ -15,7 +15,6 @@ func Read(filename string) ([]byte, error) {
 // Run will execute the named function on the wasm bytes with the passed arguments.
 // Returns the result or an error
 func Run(code []byte, call string, args []interface{}) (*wasm.Value, error) {
-
 	// Instantiates the WebAssembly module.
 	instance, err := wasm.NewInstance(code)
 	if err != nil {
@@ -28,11 +27,49 @@ func Run(code []byte, call string, args []interface{}) (*wasm.Value, error) {
 		return nil, errors.Errorf("Function %s not in Exports", call)
 	}
 
-	ret, err := f(args...)
+	fArgs := prepareArgs(instance, args)
+
+	ret, err := f(fArgs...)
 	if err != nil {
 		return nil, errors.Wrap(err, "Execution failure")
 	}
 	fmt.Printf("%v: %v\n", ret.GetType(), ret)
 
 	return &ret, nil
+}
+
+func prepareArgs(instance wasm.Instance, args []interface{}) []interface{} {
+	out := make([]interface{}, len(args))
+
+	for i, arg := range args {
+		switch t := arg.(type) {
+		case int32, int64:
+			out[i] = arg
+		case string:
+			out[i] = prepareString(instance, t)
+		// case []byte:
+		// 	out[i] = prepareBytes(instance, arg)
+		default:
+			panic(fmt.Sprintf("Unsupported type: %T", arg))
+		}
+	}
+	return out
+}
+
+func prepareString(instance wasm.Instance, t string) int32 {
+	l := len(t)
+	allocateResult, _ := instance.Exports["allocate"](l)
+	inputPointer := allocateResult.ToI32()
+
+	// Write the subject into the memory.
+	memory := instance.Memory.Data()[inputPointer:]
+
+	for nth := 0; nth < l; nth++ {
+		memory[nth] = t[nth]
+	}
+
+	// C-string terminates by NULL.
+	memory[l] = 0
+
+	return inputPointer
 }
